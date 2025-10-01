ec8f710b1ced22ad8e01b4aa0de9414c27978f22
Mariana Clarice <mclaricesilva04@gmail.com>
2025-10-01 01:21:16 -0300

    Create pop-ups with video previews on my projects' section and general adjustment to other sections

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.querySelectorAll(".slideshow").forEach(slideshow => {
-    const imgs = slideshow.querySelectorAll("img");
-    let index = 0, interval;
-
-  // mostra só a primeira imagem no carregamento
-    imgs.forEach((img, i) => img.classList.toggle("active", i === 0));
-
-    slideshow.addEventListener("mouseenter", () => {
-    interval = setInterval(() => {
-        imgs[index].classList.remove("active");
-        index = (index + 1) % imgs.length;
-        imgs[index].classList.add("active");
-    }, 1000); // troca a cada 1s
+document.querySelectorAll(".categoria").forEach(cat => {
+    cat.addEventListener("click", () => {
+        const modalId = cat.getAttribute("data-modal");
+        const modal = document.getElementById(modalId);
+        modal.style.display = "flex";
     });
+});
 
-    slideshow.addEventListener("mouseleave", () => {
-    clearInterval(interval);
-    imgs.forEach((img, i) => img.classList.toggle("active", i === 0));
-    index = 0;
+document.querySelectorAll(".modal .close").forEach(btn => {
+    btn.addEventListener("click", e => {
+        e.target.closest(".modal").style.display = "none";
+    });
+});
+
+document.querySelectorAll(".modal").forEach(modal => {
+    modal.addEventListener("click", e => {
+        if(e.target === modal) {
+            modal.style.display = "none";
+        }
     });
 });
